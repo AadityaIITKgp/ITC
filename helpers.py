5e3226bd6615fe602b3de8406ead63086b23ec13
AadityaIITKgp <45845972+AadityaIITKgp@users.noreply.github.com>
2022-09-11 22:59:39 +0530

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def find_substrings(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
